76e7aa8a490dfe65c5f0e961b554fb9f52e1a2f4
muhammedSuhail <suhailkzp1@gmail.com>
2024-07-04 14:27:42 +0530

    login with google error

--- passport.js
+++ passport.js
@@ -12,10 +12,17 @@ passport.deserializeUser(function (user, done) {
     done(null, user);
 })
 
+// passport.use(new GoogleStrategy({
+//     clientID: process.env.CLIENT_ID,
+//     clientSecret: process.env.CLIENT_SECRET,
+//     // callbackURL: 'http://localhost:3000/auth/google/callback',
+//     callbackURL: 'https://furnifusion.online/auth/google/callback',
+//     passReqToCallback: true
+
+
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    // callbackURL: 'http://localhost:3000/auth/google/callback',
     callbackURL: 'https://furnifusion.online/auth/google/callback',
     passReqToCallback: true
 
