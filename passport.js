43fdc687748c3d490412b61c79a9e2d2259835ef
muhammedSuhail <suhailkzp1@gmail.com>
2024-07-04 14:24:25 +0530

    google login https error

--- passport.js
+++ passport.js
@@ -16,8 +16,7 @@ passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     // callbackURL: 'http://localhost:3000/auth/google/callback',
-     callbackURL: 'http://furnifusion.online/auth/google/callback',
-
+    callbackURL: 'https://furnifusion.online/auth/google/callback',
     passReqToCallback: true
 
 
